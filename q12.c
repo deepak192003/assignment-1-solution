3d33ee03eb96e2133b0d317b332e466f9e55ba9f
deepak192003 <103352354+deepak192003@users.noreply.github.com>
2022-08-11 20:16:35 +0530

    Add files via upload

---
+++ q12.c
@@ -0,0 +1,10 @@
+/*Find output of below code:
+#include<stdio.h>
+int main()
+{ 
+int x = printf(“ineuron”);
+printf(“%d”,x);
+return 0;
+}*/
+
+Output of the given code is length of the string that is 7
---
+++ q5.c
@@ -0,0 +1,8 @@
+// WAP to calculate the length of String using printf function.
+#include<stdio.h>
+int main()
+{
+    int d;
+    d=printf("Deepak");
+    printf("Length of the string is %d",d);
+}
